cfe8eb0b52cd2269ca17ce473a124a8e73163ee2
mateusbra <suehtamzarb@gmail.com>
2026-02-12 18:37:19 -0300

    feat: add CORS support to the application

--- AutoflexBack/src/app.ts
+++ AutoflexBack/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { routes } from "./routes";
 import { errorHandler } from "./middlewares/errorHandler";
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 app.use(errorHandler);
 export default app;
